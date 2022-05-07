84e37a04bf0063e5d54a53229b8f806e1b6da471
azurestefan <45004561+azurestefan@users.noreply.github.com>
2022-05-07 23:53:22 +0900

    third commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,21 +7,63 @@ function Navbar() {
   const navigate = useNavigate()
   const location = useLocation()
 
+  const pathMatchRoute = (route) => {
+    if (route === location.pathname) {
+      return true
+    }
+  }
+
   return (
     <footer className='navbar'>
       <nav className='navbarNav'>
         <ul className='navbarListItems'>
           <li className='navbarListItem' onClick={() => navigate('/')}>
-            <ExploreIcon fill='#2c2c2c' width='36px' height='36px' />
-            <p>Explore</p>
+            <ExploreIcon
+              fill={pathMatchRoute('/') ? '#2c2c2c' : '#8f8f8f'}
+              width='36px'
+              height='36px'
+            />
+            <p
+              className={
+                pathMatchRoute('/')
+                  ? 'navbarListItemNameActive'
+                  : 'navbarListItemName'
+              }
+            >
+              Explore
+            </p>
           </li>
-          <li className='navbarListItem'>
-            <OfferIcon fill='#2c2c2c' width='36px' height='36px' />
-            <p>Offer</p>
+          <li className='navbarListItem' onClick={() => navigate('/offers')}>
+            <OfferIcon
+              fill={pathMatchRoute('/offers') ? '#2c2c2c' : '#8f8f8f'}
+              width='36px'
+              height='36px'
+            />
+            <p
+              className={
+                pathMatchRoute('/offer')
+                  ? 'navbarListItemNameActive'
+                  : 'navbarListItemName'
+              }
+            >
+              Offer
+            </p>
           </li>
-          <li className='navbarListItem'>
-            <PersonOutlineIcon fill='#2c2c2c' width='36px' height='36px' />
-            <p>Profile</p>
+          <li className='navbarListItem' onClick={() => navigate('/profile')}>
+            <PersonOutlineIcon
+              fill={pathMatchRoute('/profile') ? '#2c2c2c' : '#8f8f8f'}
+              width='36px'
+              height='36px'
+            />
+            <p
+              className={
+                pathMatchRoute('/profile')
+                  ? 'navbarListItemNameActive'
+                  : 'navbarListItemName'
+              }
+            >
+              Profile
+            </p>
           </li>
         </ul>
       </nav>
