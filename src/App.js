772017394dcf7f6d5c9594c720ee877d36f3cd32
azurestefan <45004561+azurestefan@users.noreply.github.com>
2022-05-10 22:23:56 +0900

    sixth commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
 import Profile from './pages/Profile'
@@ -21,6 +23,8 @@ function App() {
         </Routes>
         <Navbar /> 
       </Router>
+
+      <ToastContainer/>
     </>
   )
 }
