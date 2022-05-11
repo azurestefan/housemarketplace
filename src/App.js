0f7d9302fa0ef88e242a0a0c24a0abb8013cfd29
azurestefan <45004561+azurestefan@users.noreply.github.com>
2022-05-11 22:27:39 +0900

    seventh commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import PrivateRoute from './components/PrivateRoute'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Explore from './pages/Explore'
@@ -16,15 +17,17 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
-          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
         </Routes>
-        <Navbar /> 
+        <Navbar />
       </Router>
 
-      <ToastContainer/>
+      <ToastContainer />
     </>
   )
 }
